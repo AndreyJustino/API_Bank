5d98c9ee036a1ec3511bbb57f8474b58fa79dfda
AndreyJustino <andreyjustino2020@gmail.com>
2024-10-02 17:00:09 -0300

    feat: add codebase for controller and router user

--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
+import deleteUser from "../../controller/user/deleteUser.controller.js";
+import getUser from "../../controller/user/getUser.controller.js";
+import postUser from "../../controller/user/postUser.controller.js";
+import putUser from "../../controller/user/putUser.controller.js";
+
 const routerUser = Router()
 
-routerUser.get("/", () => {})
-routerUser.post("/", () => {})
-routerUser.delete("/", () => {})
-routerUser.put("/", () => {})
+routerUser.get("/", deleteUser)
+routerUser.post("/", getUser)
+routerUser.delete("/", postUser)
+routerUser.put("/", putUser)
 
 export default routerUser
