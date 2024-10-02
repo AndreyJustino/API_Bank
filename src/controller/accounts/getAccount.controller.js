bfd060b36669883e8cf2286bb9d4b7cd7808574a
AndreyJustino <andreyjustino2020@gmail.com>
2024-10-02 16:27:35 -0300

    feat: add codebase in controller accounts

---
+++ src/controller/accounts/getAccount.controller.js
@@ -0,0 +1,11 @@
+import { accounts } from "../../model/model.jsx";
+
+async function getAccounts(req,res) {
+    try{
+
+    }catch(error){
+        console.error("Error in getAccount: ",error.message);
+    }
+}
+
+export default getAccounts
