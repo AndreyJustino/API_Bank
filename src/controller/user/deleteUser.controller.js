00e81cc57e2b0f00094d2f9a5fd24b48700f7de5
miszael <miszaelnunescosta@gmail.com>
2024-10-08 18:11:28 -0300

    ADD deleteUser.controller

--- src/controller/user/deleteUser.controller.js
+++ src/controller/user/deleteUser.controller.js
@@ -1,17 +1,68 @@
-// Importa o modelo `user` do arquivo "model.js", que representa a tabela de usuários no banco de dados.
 import { user } from "../../model/model.js";
+import bcrypt from "bcrypt";
 
-// Define uma função assíncrona chamada `deleteUser` que será utilizada para deletar um usuário existente.
+// Define a função assíncrona `deleteUser`, que será usada para deletar um usuário existente.
 async function deleteUser(req, res) {
     try {
-        // Aqui será implementada a lógica para deletar um usuário no banco de dados.
-        // Normalmente, você obteria o ID do usuário a ser deletado dos parâmetros da requisição (req.params).
+        const userInput = req.body;
+
+        // Verifica se todos os campos obrigatórios foram preenchidos (email e senha).
+        if (!userInput.email || !userInput.password) {
+            return res.status(400).json({
+                message: "Preencha todos os campos.",
+                status: 400
+            });
+        }
+
+        // Busca o usuário no banco de dados pelo email.
+        const userDB = await user.findOne({
+            where: {
+                email: userInput.email,
+            }
+        });
+
+        // Verifica se o usuário foi encontrado.
+        if (!userDB) {
+            return res.status(404).json({
+                message: "Usuário não encontrado.",
+                status: 404
+            });
+        }
+
+        // Compara a senha fornecida com a senha criptografada no banco de dados.
+        const validarPassword = await bcrypt.compare(user.password, userDB.password);
+
+        if (!validarPassword) {
+            // Se a senha estiver incorreta, retorna uma resposta de erro.
+            return res.status(401).json({
+                message: "Senha incorreta.",
+                status: 401
+            });
+        }
+
+        // Deleta o usuário no banco de dados se a senha for válida.
+        await usuario.destroy({
+            where: {
+                email: user.email
+            }
+        });
+
+        // Retorna uma resposta de sucesso após deletar o usuário.
+        return res.status(200).json({
+            message: "Usuário deletado com sucesso.",
+            status: 200
+        });
+
     } catch (error) {
-        // Se ocorrer qualquer erro durante a execução, ele será capturado e registrado no console com uma mensagem descritiva.
+        // Captura e trata qualquer erro que ocorra durante a execução.
         console.error("Error in deleteUser: ", error.message);
+        return res.status(500).json({
+            message: "Erro ao deletar usuário.",
+            erro: error.message,
+            status: 500
+        });
     }
 }
 
-// Exporta a função `deleteUser` para que ela possa ser utilizada em outros arquivos,
-// como nas rotas ou controladores da API.
+
 export default deleteUser;
