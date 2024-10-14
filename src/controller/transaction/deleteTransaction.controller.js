eab3a3791f6196d0d90f0c3ae8488e47912eae29
AndreyJustino <andreyjustino2020@gmail.com>
2024-10-14 09:40:59 -0300

    style: cleaning code

--- src/controller/transaction/deleteTransaction.controller.js
+++ src/controller/transaction/deleteTransaction.controller.js
@@ -1,18 +1,11 @@
-// Importa o modelo `transactions` do arquivo "model.js", que representa a tabela de transações no banco de dados.
 import { transactions } from "../../model/model.js";
 
-// Define uma função assíncrona chamada `deleteTransaction` que será utilizada para deletar uma transação existente.
 async function deleteTransaction(req, res) {
     try {
-        // Aqui o código para deletar a transação no banco de dados será implementado.
-        // Normalmente, você pode obter o ID da transação a ser deletada dos parâmetros da requisição (req.params).
+
     } catch (error) {
-        // Se ocorrer qualquer erro durante a execução, ele será capturado aqui.
-        // O erro será registrado no console.
         console.error(error);
     }
 }
 
-// Exporta a função `deleteTransaction` para que ela possa ser utilizada em outros arquivos,
-// como nas rotas ou controladores da API.
 export default deleteTransaction;
--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,33 +1,20 @@
-// Importa o objeto `Router` do Express, que permite criar rotas de forma modular e organizada.
 import { Router } from "express";
 
-// Importa os controladores responsáveis pelas operações sobre os usuários.
-// Cada um desses controladores contém a lógica para realizar as operações de CRUD (Criar, Ler, Atualizar e Deletar) em usuários.
 import deleteUser from "../../controller/user/deleteUser.controller.js";
 import getUser from "../../controller/user/getUser.controller.js";
 import postUser from "../../controller/user/postUser.controller.js";
 import putUser from "../../controller/user/putUser.controller.js";
 import postLogin from "../../controller/user/postLogin.js";
 
-// Cria uma instância do roteador, onde serão configuradas as rotas para as operações relacionadas aos usuários.
 const routerUser = Router();
 
-// Define uma rota HTTP GET para buscar informações sobre usuários.
-// Quando uma requisição GET for feita para "/getUser", o controlador `getUser` será executado.
 routerUser.get("/getUser/:email", getUser);
 
-// Define uma rota HTTP POST para criar um novo usuário.
-// Quando uma requisição POST for feita para "/postUser", o controlador `postUser` será executado.
 routerUser.post("/postUser", postUser);
 routerUser.post("/loginUser", postLogin);
 
-// Define uma rota HTTP DELETE para deletar um usuário.
-// Quando uma requisição DELETE for feita para "/deleteUser", o controlador `deleteUser` será executado.
 routerUser.delete("/deleteUser", deleteUser);
 
-// Define uma rota HTTP PUT para atualizar informações de um usuário.
-// Quando uma requisição PUT for feita para "/putUser", o controlador `putUser` será executado.
 routerUser.put("/putUser/:id", putUser);
 
-// Exporta o roteador configurado para que ele possa ser usado em outros arquivos da aplicação, como na definição da API principal.
 export default routerUser;
