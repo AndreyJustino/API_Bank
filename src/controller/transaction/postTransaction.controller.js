1e783e4de5e2a6916cdcf7ac1e955bf94f2a7f27
David Neves <davidoneves563@gmail.com>
2024-10-14 12:16:43 -0300

    feat: removing description and amount

--- src/controller/transaction/postTransaction.controller.js
+++ src/controller/transaction/postTransaction.controller.js
@@ -3,18 +3,16 @@ import { transactions } from "../../model/model.js";
 async function postTransaction(req, res) {
     try {
         // Extraindo os dados da transação do corpo da requisição
-        const { amount, type, description, accountId } = req.body;
+        const { type, accountId } = req.body;
 
         // Verifica se todos os campos obrigatórios foram fornecidos e não são valores vazios.
-        if (!amount || !type || !accountId || accountId.trim() === "") {
+        if (!type || !accountId || accountId.trim() === "") {
             return res.status(400).json({ message: "Todos os campos obrigatórios devem ser preenchidos e não podem estar vazios." });
         }
 
         // Cria uma nova transação no banco de dados
         const newTransaction = await transactions.create({
-            amount,
             type,
-            description,
             accountId
         });
 
